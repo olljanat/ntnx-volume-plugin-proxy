c33a0f59c34906bdb3997911c3d36cdde597dab9
Olli Janatuinen <olli.janatuinen@gmail.com>
2021-04-07 11:20:16 +0300

    Handle volume groups without disks correctly

--- scripts/volume_groups.sh
+++ scripts/volume_groups.sh
@@ -7,4 +7,4 @@ printf "Content-Type: application/json\n\n"
 # so we use "jq" to filter results before returning them to client
 curl --silent --header "Authorization: Basic $AuthorizationBase64" \
 --tlsv1.2 --insecure "https://$UPSTREAM/api/nutanix/v0.8/volume_groups?includeDiskSize=True" \
-| jq "[.[] | select(.diskList[0].containerId | contains($REMOTE_USER))]" | cat
+| jq "[.[] | select(.diskList[0]) | select(.diskList[0].containerId | contains($REMOTE_USER))]" | cat
